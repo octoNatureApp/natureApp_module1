8a831b72144df4597e4446be4b0a0762a84782a3
Michelle Ross <miross629@gmail.com>
2022-12-12 12:02:23 -0800

    Get DOM elements and add nav to create profile index pg

---
+++ create-profile/create-profile.js
@@ -0,0 +1,17 @@
+// imports
+
+// get DOM elements
+const errorDisplay = document.getElementById('error-display');
+const preview = document.getElementById('preview');
+const profileForm = document.getElementById('profile-form');
+const updateButton = profileForm.querySelector('button');
+const userNameInput = profileForm.querySelector('[name=username]');
+const avatarInput = profileForm.querySelector('[name=avatar]');
+const headlineInput = profileForm.querySelector('[name=headline]');
+const signOutLink = document.getElementById('sign-out-link');
+
+// state
+
+// events
+
+// display
