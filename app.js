ceec26c197a955e033aad34c838e09263b5a7a91
Akilah Powell <atpjun@gmail.com>
2022-12-12 15:07:56 -0400

    created the app js function and event listeners

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 /* Imports */
 // this will check if we have a user and set signout link if it exists
 import './auth/user.js';
+import { getProfiles } from './fetch-utils.js';
+import { renderProfile } from './render-utils.js';
 
 /* Get DOM Elements */
+const containerEl = document.getElementById('profiles-container');
 
 /* State */
 
 /* Events */
+window.addEventListener('load', async () => {
+    displayProfiles();
+});
 
 /* Display Functions */
+async function displayProfiles() {
+    containerEl.textContent = '';
+
+    const profiles = await getProfiles();
+    console.log(profiles);
+
+    for (let profile of profiles) {
+        const profileEl = renderProfile(profile);
+        containerEl.append(profileEl);
+    }
+}
