0b79d582d53da6dfcb3d8a0059b24f306123a466
Michelle Ross <miross629@gmail.com>
2022-12-13 10:23:33 -0800

    Add event listener for post submit. Not working.

---
+++ create-post/create-post.js
@@ -0,0 +1,65 @@
+// import
+
+import { getUser, uploadNaturePic, upsertPost } from '../fetch-utils.js';
+
+// get DOM elements
+const errorDisplay = document.getElementById('error-display');
+const preview = document.getElementById('preview');
+const postForm = document.getElementById('create-post-form');
+const updateButton = postForm.querySelector('button');
+const altTextInput = postForm.querySelector('[name=alt-text]');
+const locationInput = postForm.querySelector('[name=location]');
+const descriptionInput = postForm.querySelector('[name=description]');
+const signOutLink = document.getElementById('sign-out-link');
+
+// events
+let error = null;
+let post = null;
+
+const user = getUser();
+
+console.log('postForm', postForm);
+
+// display
+// post form submit button event listener
+postForm.addEventListener('submit', async (e) => {
+    console.log('hello!');
+    e.preventDefault();
+
+    updateButton.disabled = true;
+    updateButton.textContent = 'Posting...';
+
+    const formData = new FormData(postForm);
+    console.log('formData', formData);
+
+    // object: altText, location, description
+    const postObj = {
+        alt_text: formData.get('alt-text'),
+        location: formData.get('location'),
+        description: formData.get('description'),
+    };
+
+    // get image file from form
+    const imageFile = formData.get('nature');
+
+    if (imageFile.size) {
+        const imagePath = `${user.id}/${imageFile.name}`;
+        const url = await uploadNaturePic(imagePath, imageFile);
+
+        postObj.naturepic_url = url;
+    }
+
+    // upsert
+    const response = await upsertPost(postObj);
+    console.log('response', response);
+
+    error = response.error;
+
+    if (error) {
+        errorDisplay.textContent = `There was an error: ${error.message}`;
+        updateButton.disabled = false;
+        updateButton.textContent = 'POST';
+    } else {
+        location.assign('./profile-feed/');
+    }
+});
